26fdb2ce47f97383dac3c2ed9a9b508f60477201
Lewiszz <lewis@trueprice.org>
2023-09-21 13:54:45 +0200

    Read weight from test csv with animals

--- Program.fs
+++ Program.fs
@@ -1,2 +1,11 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open Deedle
+open FSharp.Data
+
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+let animals = Frame.ReadCsv("./test.csv", hasHeaders=true, separators=",")
+
+let weight = animals.GetColumn<float>("weight_in_kg")
+
+printfn "Weight: %A" weight
