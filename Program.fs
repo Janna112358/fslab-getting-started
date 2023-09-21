d6046f5bde267eb1d949dccd61057430b08b234b
Janna Goldstein <janna@impactinstitute.com>
2023-09-21 16:01:48 +0200

    i made a plot O.o

--- Program.fs
+++ Program.fs
@@ -1,5 +1,6 @@
 ﻿open FSharp.Data
 open Deedle
+open Plotly.NET
 
 // Retrieve data using the FSharp.Data package
 let rawData =
@@ -7,6 +8,26 @@ let rawData =
 
 // And create a data frame object using the ReadCsvString method provided by Deedle.
 // Note: Of course you can directly provide the path to a local source.
-let dataFrame = Frame.ReadCsvString(rawData, hasHeaders = true, separators = "\t")
+let housesFull = Frame.ReadCsvString(rawData, hasHeaders = true, separators = "\t")
 
-dataFrame.Print true
+// get dataframe with only some of the columns, and only houses not on the river
+let housesNotAtRiver =
+    let targetColumns =
+        seq {
+            "RoomsPerDwelling"
+            "MedianHomeValue"
+            "CharlesRiver"
+        }
+
+    housesFull
+    |> Frame.sliceCols targetColumns
+    |> Frame.filterRowValues (fun x -> x.GetAs<bool>("CharlesRiver") |> not)
+
+
+let pricesNotAtRiver: seq<float> =
+    housesNotAtRiver.GetColumn "MedianHomeValue" |> Series.values
+
+Chart.Histogram(pricesNotAtRiver)
+|> Chart.withYAxisStyle ("median value of owner occupied home in 1000s")
+|> Chart.withXAxisStyle ("price distribution")
+|> Chart.show
