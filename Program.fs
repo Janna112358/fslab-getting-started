f2d529bb7e0d4fa0fb2c955a515636c855217a6e
Janna Goldstein <janna@impactinstitute.com>
2023-09-21 13:59:57 +0200

    try basic program

--- Program.fs
+++ Program.fs
@@ -1,2 +1,10 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open FSharp.Data
+open Deedle
+
+// Retrieve data using the FSharp.Data package
+let rawData =
+    Http.RequestString @"https://raw.githubusercontent.com/dotnet/machinelearning/master/test/data/housing.txt"
+
+// And create a data frame object using the ReadCsvString method provided by Deedle.
+// Note: Of course you can directly provide the path to a local source.
+let df = Frame.ReadCsvString(rawData, hasHeaders = true, separators = "\t")
